dd206cac3a9d27662f2baca6e10574c0b87b4976
128jcw <101718441+128jcw@users.noreply.github.com>
2022-10-20 09:59:23 +0900

    Add files via upload

--- lab_project/connect/OracleJdbc.java
+++ lab_project/connect/OracleJdbc.java
@@ -2,7 +2,7 @@ package edu.java.project.connect;
 
 public interface OracleJdbc {
  // Oracle DB에 접속하는 주소(, 포트번호, SID)를 정의
-    String URL="jdbc:oracle:thin:@localhost:1521:xe";
+    String URL="jdbc:oracle:thin:@192.168.20.2:1521:xe";
     
     // Oracle DB에 접속하는 사용자 계정
     String USER="scott";
